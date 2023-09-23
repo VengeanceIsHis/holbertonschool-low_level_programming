be9613297fa99162d9915a9256e1f58cfff7ce03
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-23 09:57:20 -0700

    fix

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-
+/**
+*main - entry point
+*Return: Always 0
+*/
 
 int main(void)
 {
-  char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-  int i;
-  for (i = 0; i< 26; i++)
-    {
-      putchar(alp[i]);
-    }
-      putchar('\n');
-  return (0);
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i< 26; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
 }
