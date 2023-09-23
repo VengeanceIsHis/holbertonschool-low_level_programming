e0cc3672cf6c2fe8735362fbc07578a40d4711b4
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-23 11:50:49 -0700

    fix

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - prints alphabet in reverse
+*Return: Always 0
+*/
+
+int main(void)
+{
+char alp[26] = "zyzwvutsrqponmlkjihgfedcba";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
