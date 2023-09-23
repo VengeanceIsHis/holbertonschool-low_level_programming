e2ebf03ef88331477dc2f55152b947981162227e
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-23 09:32:26 -0700

    Fin

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/**
+*main - entry point
+*return: Always 0
+*/
 
 int main(void)
 {
@@ -14,5 +17,5 @@ else if ((n % 10) < 6 && (n % 10) != 0)
 { printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10); }
 else
 { printf("Last digit of %d is %d and is 0\n", n, n % 10); }
-return(0);
+return (0);
 }
