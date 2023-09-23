955c0a202f0ee5202c2bdc60c1e860db60c4e2f6
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-23 12:59:40 -0700

    fix

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main- prints single digit numbers with ',' and spaces between each integer
+ *Return: Always 0
+ */
+
+int main(void)
+{
+int n;
+for (n = 38; n < 48; n++)
+{
+putchar(n);
+if (n != 47)
+{
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
