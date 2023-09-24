ded943a07795367ae68d813bf0e508920ab0c928
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-24 16:35:31 -0700

    fix

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  *print_sign - prints the signs according to each number
+ *@n: variable for numbers in function
  *Return: 1 for positive, 0 for 0, -1 for negative numbers
  */
 int print_sign(int n)
