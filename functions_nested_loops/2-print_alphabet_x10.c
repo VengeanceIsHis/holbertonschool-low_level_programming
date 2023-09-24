96403c41c81780e869830af6fc818ca707fe39ed
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-24 11:27:59 -0700

    fix

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,8 +3,18 @@
 
 void print_alphabet_x10(void)
 {
-char letter
-  for (letter = 'a'; letter <= 'z'; letter++)
-    _putchar(letter);
- _putchar('\n');
+  char ch;
+  int i;
+  i = 0
+    while (i < 10)
+      {
+       ch ='a'
+       while (ch <='z')
+	 {
+	  _putchar(ch);
+	  ch++;
+	 }
+       _putchar('\n');
+       i++;
+      }
 }
