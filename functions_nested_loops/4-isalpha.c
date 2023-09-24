e7e9327915d015effd5359d845e6091fc7088359
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-24 15:39:50 -0700

    1ATT

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_isalpha checks for all letters lowercase and uppercase
+ *@c: The character that needs to be checked
+ *Return: 1 for a letter, 0 if it isn't
+*/
+
+int _isalpha(int c);
+{
+if (c >= 65 && c <= 122)
+{
+return (1);
+}
+return (0);
