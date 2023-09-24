ea1fdf0396484ede179f7aea160bcb4061afaf9e
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-24 16:43:06 -0700

    1ATT

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_abs - outputs absolute value of any given number
+ *@n: the integer to check
+ *Return: the absolute value
+ */
+
+int abs(int)
+{
+if (n >= 0)
+{
+return (n);f
+}
+return (-n);
+}
