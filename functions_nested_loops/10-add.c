bf468b45d616727954bd5894fca0048779159843
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-24 17:59:15 -0700

    1ATT

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main"
+/**
+ *add -adds two integers and returns result
+ *@i: integer to be added to i
+ *@j: integer to be added to j
+ *Return: sum of a and b
+ */
+int add(int i, int j)
+{
+int k;
+k = i + j;
+return (k);
+}
