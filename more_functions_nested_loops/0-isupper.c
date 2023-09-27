6e199415a4ab1bc590a81cfcaed9e78666098f9d
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-27 10:23:00 -0700

    got it

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * _isupper - checks for upper case letters
+ *@c: ASCII number to check for upper case letters
+ *Return: 1 for upper case letters 0 for everything else
  */
 int _isupper(int c)
 {
