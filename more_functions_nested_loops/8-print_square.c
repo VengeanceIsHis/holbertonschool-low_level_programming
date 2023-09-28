e50b2f79eb7032db750f51d9902fb2169f9831f6
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-28 16:29:25 -0700

    test

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void print_square(int size)
+{
+  int x;
+  if (size <= 0)
+    {
+      _putchar('\n');
+    }
+  else
+    {
+      for (x = 0; x < n; x++)
+	{
+	  _putchar(35);
+	  _putchar(35);
+	}
+      _putchar('\n')
+	}
+}
