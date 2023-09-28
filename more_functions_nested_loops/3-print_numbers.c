ba62672508a300598d05cd85dd785d834f35d0f3
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-28 09:54:57 -0700

    test

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -4,10 +4,9 @@
 void print_numbers(void)
 {
   char c;
-  for (c = 0; c <= 9; c++)
-{
-  _putchar(c);
-}
- _putchar("\n");
- return (0);
+  for (c = '0'; c <= '9'; c++)
+    {
+      _putchar(c);
+    }
+  _putchar('\n');
 }
