012239a93c6e9ca1bade7cf1d47dcf700027e2dd
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-28 15:05:48 -0700

    betty check

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,18 +4,17 @@
  */
 void more_numbers(void)
 {
-  int a, b;
-  for (a = 0; a < 10; a++)
+int a, b;
+for (a = 0; a < 10; a++)
 {
-  for (b = 0; b < 15; b++)
-    {
-      if (b > 9)
-	{
-	  _putchar((b / 10) + '0');
-	}
-	  _putchar((b % 10) + '0');  
-	}
-
-  _putchar('\n');
+for (b = 0; b < 15; b++)
+{
+if (b > 9)
+{
+_putchar((b / 10) + '0');
+}
+_putchar((b % 10) + '0');  
+}
+_putchar('\n');
 }
 }
