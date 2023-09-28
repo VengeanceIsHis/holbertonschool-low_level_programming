7be6708b697e6a4773c9f653b885346a0871c715
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-28 10:16:14 -0700

    test

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,12 @@
 #include "main.h"
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
   char c = 0
     while (c <= 9)
       {
        if (c != 2 && c != 4)
-
-	 
-	  _putchar(c)
+	 _putchar(c)
+	   c++;
 	 }
   _putchar('\n')
     }
