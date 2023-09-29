5393618bbe9e6d6e9b881ab71c7510109bc80da6
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-28 17:57:24 -0700

    test

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+  for (x = 1; x <= 100; x++)
+      if (x % 3 == 0)
+	{
+	printf("Fizz");
+
+	}
+      else if
+	{
+	  (x % 3 == 0 || x % 5 == 0)
+	  printf("Fizz Buzz");
+	}
+      else if
+	{
+	    (x % 5 == 0)
+	    printf("Buzz");
+	}
+	    else
+	      {
+		printf("%d", x);
+	      }
+}
