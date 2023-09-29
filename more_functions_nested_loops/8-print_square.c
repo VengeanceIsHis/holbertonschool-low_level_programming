c4127a6acf83447c50d09727af57c517e0304134
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-28 17:40:17 -0700

    test

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -5,20 +5,20 @@
  */
 void print_square(int size)
 {
-  int x, y;
-  y = 0;
-  if (size < 1)
-      _putchar('\n');
-      while (y < size)
+  if (size <= 0)
     {
-      x = 0;
-      while (x < size)
+      _putchar('\n');
+    }
+  else
+    {	
+      int i, j;
+      for (i = 0; i < size; i++)
 	{
-	  x = 0;
+	  for (j = 0; j < size; j++)
+	    {
 	  _putchar('#');
-	  x++;
 	}
       _putchar('\n');
-      y++;
+	}
 	}
 }
