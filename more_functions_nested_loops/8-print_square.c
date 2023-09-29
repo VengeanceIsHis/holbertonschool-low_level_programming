c027227304d1efbc2eda6fb56779b01f6c7c7653
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-28 17:45:47 -0700

    betty fix

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,25 @@
 #include "main.h"
 /**
- *
- *
+ *print_square - prints square based on the size that you give
+ *Return: No return
+ *@size: input to determine if there is a square and the size of the square.
  */
 void print_square(int size)
 {
-  if (size <= 0)
-    {
-      _putchar('\n');
-    }
-  else
-    {	
-      int i, j;
-      for (i = 0; i < size; i++)
-	{
-	  for (j = 0; j < size; j++)
-	    {
-	  _putchar('#');
-	}
-      _putchar('\n');
-	}
-	}
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{	
+int i, j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
 }
