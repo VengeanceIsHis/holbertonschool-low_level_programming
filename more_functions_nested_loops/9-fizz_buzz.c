df775cd072992debf670d498f1adf9043ba1bb99
VengeanceIsHis <elijahjpoole@gmail.com>
2023-09-28 18:18:46 -0700

    fix

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,20 +2,19 @@
 #include "main.h"
 int main(void)
 {
-
-  int x;
-  for (x = 1; x <= 100; x++)
-    {
-      if ((x % 3 == 0) && (x % 5 == 0))
-	{
-	printf("FizzBuzz");
+int x;
+for (x = 1; x <= 100; x++)
+{
+if ((x % 3 == 0) && (x % 5 == 0))
+{
+printf("FizzBuzz");
 }
 else if (x % 3 == 0)
-  {
+{
 printf("Fizz");
 }
 else if (x % 5 == 0)
-  {
+{
 printf("Buzz");
 }
 else
