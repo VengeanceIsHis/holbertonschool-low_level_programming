7b28b78d7c74767abb18440a6515a415585323a9
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-01 10:34:54 -0700

    test

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,7 +9,7 @@ void puts2(char *str)
 int len = 0;
 int t = 0;
 char *y = str;
-int 0;
+int o;
 
 while (*y != '\0')
 {
@@ -19,7 +19,7 @@ len++;
 t = len - 1;
 for (o = 0; o <= t; o++)
 {
-if o % 2 == 0)
+if (o % 2 == 0)
 {
 _putchar(str[o]);
 }
