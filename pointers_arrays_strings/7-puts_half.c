b0c34dee48b18d2f1ae2e933672c2474f405e3af
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-01 10:48:38 -0700

    test

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- *
- *
- *
+ *puts_half - prints half of a string
+ *@str: string used
+ *Return: half of string
  */
 void puts_half(char *str)
 {
-  int a, n, len;
-  len = 0;
-  for (a = 0; str[a] != '\0'; a++)
-    len++;
-  n = (len / 2);
-  if ((len % 2) == 1)
-    n = ((len + 1) / 2);
-  for (a = n; str[a] != '\0'; a++)
-    _putchar(str[a]);
-  _putchar('\n');
+int a, n, len;
+len = 0;
+for (a = 0; str[a] != '\0'; a++)
+len++;
+n = (len / 2);
+if ((len % 2) == 1)
+n = ((len + 1) / 2);
+for (a = n; str[a] != '\0'; a++)
+_putchar(str[a]);
+_putchar('\n');
 }
