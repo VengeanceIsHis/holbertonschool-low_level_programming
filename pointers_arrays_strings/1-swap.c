dcc9a1960e870e51748fbc7fd93a376c6ce7a9cd
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-01 09:24:26 -0700

    test

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,9 +1,13 @@
 #include "main.h"
-
+/**
+ *swap_int - uses a third integet to swap two inputted integers
+ *@a: first integer to be swapped
+ *@b:second integer to be swapped
+ */
 void swap_int(int *a, int *b)
 {
-  int m;
-  m = *a;
-  *a = *b;
+int m;
+m = *a;
+*a = *b;
 *b = m;
 }
