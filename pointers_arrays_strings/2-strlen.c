87c4bf7ce616e1ba2b419f0f565b2869e151b987
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-01 09:42:17 -0700

    test

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,16 @@
 #include "main.h"
-
+/**
+ *_strlen - prints the length of string
+ *@s: string
+ *Return: length
+ */
 int _strlen(char *s)
 {
-  int str = 0;
-  while(*s != '\0')
-    {
-      str++;
-      s++;
-    }
-  return (str);
+int str = 0;
+while(*s != '\0')
+{
+str++;
+s++;
+}
+return (str);
 }
