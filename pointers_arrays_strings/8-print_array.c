30b35809e1b9df7be3f29510fc4a131837dc25bc
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-01 10:59:46 -0700

    bettchecked

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_array- prints n elements of an array
+ *@a: array name
+ *@n: is the number of elements to be printed
+ *Return: n number of elements of an array
+ */
+void puts_half(char *str)
+{
+int i;
+for (i = 0; i < (n - 1); i++)
+{
+printf("%d, ", a[i]);
+}
+if (i == (n - 1))
+{
+printf("%d", a[n - 1]);
+}
+printf("\n");
+}
