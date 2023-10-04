14ccd1ffeeb1007d64717a0717e1e0deafd94fbb
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-04 12:12:17 -0700

    test

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+  int str1;
+  int str2;
+  str1 = 0;
+  if (dest[str1] <= n)
+    {
+  while (dest[str1] != '\0')
+    {
+      str++;
+    }
+  while (src[str2] != '\0')
+    {
+      dest[str1] = src[str2];
+      str1++;
+      str2++;
+    }
+  dest[str1] = '\0';
+  return (dest);
+    }
+}
