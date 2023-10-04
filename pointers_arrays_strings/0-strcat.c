517bfd7d5b6f80271f2576d027a79ac7aaf0c49b
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-04 10:28:08 -0700

    betty fix

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,27 @@
 #include "main.h"
 /**
- *
- *
- */
+ *_strcat - concatenates two strings
+ *@dest: first input value
+ *@src: second input value
+ *Return: void
+*/
 char *_strcat(char *dest, char *src)
 {
-  int str1;
-  int str2;
+int str1;
+int str2;
 
-  str1 = 0;
-  while (dest[str1] != '\0')
-    {
-      str1++;
-    }
-  str2 = 0;
-  while (src[str2] != '\0')
-    {
-      dest[str1] = src[str2];
-      str1++;
-      str2++;
-    }
-  dest[str1] = '\0';
-  return (dest);
+str1 = 0;
+while (dest[str1] != '\0')
+{
+str1++;
+}
+str2 = 0;
+while (src[str2] != '\0')
+{
+dest[str1] = src[str2];
+str1++;
+str2++;
+}
+dest[str1] = '\0';
+return (dest);
 }
