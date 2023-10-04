806b06f4dbf720b30300f7870f5e69aa028600dc
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-04 13:54:22 -0700

    betty fix

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,27 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ *_strncat - prints out 2 strings while on the second string prints out a certain amount.
+ *@dest: 1st input value
+ *@src: 2nd input value
+ *@n: value that shows the amount of elements are printed
+ *Return: void
  */
 char *_strncat(char *dest, char *src, int n)
 {
-  int str1;
-  int str2;
-  str1 = 0;
-  while (dest[str1] != '\0')
-    {
-      str1++;
-    }
-  str2 = 0;
-  while (str2 < n && src[str2] != '\0')
-    {
-      dest[str1] = src[str2];
-      str1++;
-      str2++;
-    }
-  dest[str1] = '\0';
-  return (dest);
-    }
+int str1;
+int str2;
+str1 = 0;
+while (dest[str1] != '\0')
+{
+str1++;
+}
+str2 = 0;
+while (str2 < n && src[str2] != '\0')
+{
+dest[str1] = src[str2];
+str1++;
+str2++;
+}
+dest[str1] = '\0';
+return (dest);
+}
