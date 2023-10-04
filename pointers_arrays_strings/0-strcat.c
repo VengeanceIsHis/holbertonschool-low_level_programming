3e552440a49131c356f1eddfbb2059182006a91b
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-04 10:18:17 -0700

    git push

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+  int str1;
+  int str2;
+
+  str1 = 0;
+  while (dest[str1] != '\0')
+    {
+      str1++;
+    }
+  str2 = 0;
+  while (src[str2] != '\0')
+    {
+      dest[str1] = src[str2];
+      str1++;
+      str2++;
+    }
+  dest[str1] = '\0';
+  return (dest);
+}
