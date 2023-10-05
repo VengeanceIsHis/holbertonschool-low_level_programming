661bde5db5bc6d8f7d1cb75f22ef175de7142590
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-05 14:13:21 -0700

    betty fix

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ *_strcmp - compares two strings and prints the result unless equal
+ *@s1: input value 1
+ *@s2: input value 2
+ *Return: the difference of two inputs.
  *
  */
 int _strcmp(char *s1, char *s2)
 {
-  int i;
+int i;
 
-  i = 0;
-  while (s1[i] != '\0' && s2[i] != '\0')
-    {
-      if (s1[i] != s2[i])
-	{
-	  return (s1[i] - s2[i]);
-	}
-      i++;
-    }
-  return (0);
+i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+}
+i++;
+}
+return (0);
 }
