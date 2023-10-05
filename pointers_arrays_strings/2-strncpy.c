7c7d3bdf6d76003ca7fb5f652e5529817d9fadae
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-05 13:58:44 -0700

    betty fix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ *_strncpy - function to copy a string
+ *@dest: input value 1
+ *@src: input value 2
+ *@n: input value 3
+ *Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-  int j;
-  j = 0;
-  
-  while (j < n && src[j] != '\0')
-    {
-      dest[j] = src[j];
-	j++;
-    }
-  while (j < n)
-    {
-      dest[j] = '\0';
-      j++;
-    }
-  return (dest);
+int j;
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j];
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+return (dest);
 }
