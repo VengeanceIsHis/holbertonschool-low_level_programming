98e2d42124aef4aa24b7e1ff6916806a1815c1cb
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-07 09:31:41 -0700

    test

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+void reverse_array(int *a, int n)
+{
+  int i;
+  while (*a[i] = '\0' && i <= n)
+    {
+      i++;
+    }
+  while (*a[i] <= 0)
+    {
+      putchar(*a[i])
+	i--;
+    }
+}
