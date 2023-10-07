03866db60a2b3c0d8df8cb64a8832065587b9041
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-07 10:26:11 -0700

    betty fix

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *string_toupper
+ *string_toupper - changes lowercase letters in string to uppercase 
  *@n: pointer
  *Return: n
  */
@@ -11,7 +11,7 @@ i = 0;
 while (n[i] != '\0')
 {
 if (n[i] >= 'a' && n[i] <= 'z')
-n[i] = n[i] -32;
+n[i] = n[i] - 32;
 i++;
 }
 return (n);
