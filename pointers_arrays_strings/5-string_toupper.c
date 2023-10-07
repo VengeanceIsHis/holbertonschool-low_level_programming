fc11211e0cf156d4d0dd3819a0192656a9ba6577
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-07 10:24:07 -0700

    betty check

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  *string_toupper
- *n: pointer
+ *@n: pointer
  *Return: n
  */
 char *string_toupper(char *n)
 {
 int i;
 i = 0;
-while(n[i] != '\0')
+while (n[i] != '\0')
 {
 if (n[i] >= 'a' && n[i] <= 'z')
 n[i] = n[i] -32;
