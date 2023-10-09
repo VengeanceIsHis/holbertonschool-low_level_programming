ab3b49fb158362fadf0411c158554f197bbc8c9e
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-08 19:03:41 -0700

    betty fix

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -4,15 +4,15 @@
  *@a: array
  *Return: 0
  */
-void print_chessboard(char (a)[8])
+void print_chessboard(char (*a)[8])
 {
-  int i, j;
-  for (i = 0; i < 8; i++)
-    {
-      for (j = 0; j < 8; j++)
-	{
-	  putchar(a[i][j]);
-	}
-      putchar('\n');
-    }
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+putchar(a[i][j]);
+}
+putchar('\n');
+}
 }
