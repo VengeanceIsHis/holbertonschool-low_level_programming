85123af58cd18bca36bec7d823246d27bd4e3b6b
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-08 19:18:21 -0700

    Betty checked

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -7,16 +7,16 @@
  */
 void print_diagsums(int *a, int size)
 {
-int sum1, sum2, a;
+int sum1, sum2, b;
 sum1 = 0;
 sum2 = 0;
-for (a = 0; a < size; a++)
+for (b = 0; b < size; b++)
 {
-sum1 = sum1 + a[a * size + a];
+sum1 = sum1 + a[b * size + b];
 }
-for (a = size - 1; a >= 0; a--)
+for (b = size - 1; b >= 0; b--)
 {
-sum2 += a[y * size + (size - y -1)];
+sum2 += a[b * size + (size - b -1)];
 }
 printf"%d, %d\n", sum1, sum2);
 }
