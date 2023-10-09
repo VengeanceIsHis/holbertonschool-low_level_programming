5051009a3ca96a4666b5ab456f5cb3b67c2f6c17
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-08 19:20:52 -0700

    fox

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -17,7 +17,7 @@ sum1 = sum1 + a[b * size + b];
 }
 for (b = size - 1; b >= 0; b--)
 {
-sum2 += a[b * size + (size - b -1)];
+sum2 += a[b * size + (size - b - 1)];
 }
 printf("%d, %d\n", sum1, sum2);
 }
