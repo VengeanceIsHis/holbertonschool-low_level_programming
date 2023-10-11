0a015bef3a3411fb17fde8db7d5b82774fbf811d
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-11 10:49:21 -0700

    test

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*
+*
+*
+*/
+void _print_rev_recursion(char *s)
+{
+  int len = 0;
+  int o;
+  while (*s != '\0')
+    {
+      len++;
+      s++;
+    }
+  s--;
+  for (o = len; 0 > 0; o--)
+    {
+      _putchar(*s);
+      s--;
+    }
+  _putchar('\n');
+  _print_rev_recursion(s)
+    }
