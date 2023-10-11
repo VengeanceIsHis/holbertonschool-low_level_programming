40ada31953953b0d5a5c4adc4cf5841b3e037352
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-11 10:23:50 -0700

    betty fix

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,10 +1,16 @@
 #include "main.h"
 /**
- *
- *
+ *_puts_recursion - recursive function that prints a string.
+ *@s: string input
+ *Return: void
  */
 void _puts_recursion(char *s)
 {
-  _putchar("%c", *s);
-    _putchar("\n");
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar("\n")
 }
