136dac2551e7c3a5b5efa8e18c143fe17fea0962
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-11 10:35:02 -0700

    test

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,11 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s)
+if (*s == '\0')
 {
-  _putchar(char *s);
-_puts_recursion(s + 1);
+_putchar('\n');
+return;
 }
-else
-_putchar("\n");
+_putchar(*s);
+s++;
+_puts_recursion(s);
 }
