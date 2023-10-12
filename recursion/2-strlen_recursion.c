f4df5389033ea63df6a247c0e9403bd78c944b0f
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-12 15:15:54 -0700

    betty fix

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- *
- *
- *
+ *_strlen_recursion - returns the length of a string
+ *@s: input value
+ *Return: the length of string
  */
 int _strlen_recursion(char *s)
 {
- if (*s == '\0')
-  {
-    return (0);
-    }
- s++;
- return (_strlen_recursion(s) + 1);
+if (*s == '\0')
+{
+return (0);
+}
+s++;
+return (_strlen_recursion(s) + 1);
 }
