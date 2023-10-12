de9c93813f07ab934d833655f6cbb25f92e5be4c
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-12 16:28:58 -0700

    test

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ *_pow_recursion - returns the value of x to the power of y
+ *@x: input value
+ *@y: input value
+ *Return: The return which is the value
  */
 int _pow_recursion(int x, int y)
 {
-  if (y == 0)
-    return (1);
-  if (y < 0)
-    return (-1);
-  
-  return (x * _pow_recursion(x, y - 1));
+if (y == 0)
+return (1);
+if (y < 0)
+return (-1);
+return (x * _pow_recursion(x, y - 1));
 }
