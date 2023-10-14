1e21198c6206ce323082bcf1b29a81ac0a71d32c
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-14 08:31:05 -0700

    test

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*
+*
+*
+*
+*
+*/
+int _sqrt_recursion(int n)
+{
+  int i = 0;
+  int j = n / 2;
+  while (j != i)
+    {
+      i = j;
+      j = (a / i + i) / 2;
+    }
+  return (j);
+  _sqrt_recursion(n)
+    }
--- recursion/main.h
+++ recursion/main.h
@@ -1,4 +1,4 @@
-#ifndef main_h
+#0;276;0cifndef main_h
 #define main_h
 void _puts_recursion(char *s);
 int _putchar(char c);
@@ -6,4 +6,5 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 #endif
