939fafeb35784c2a20e26fca209cdf8480db11ec
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-14 11:04:41 -0700

    main add

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -13,3 +13,11 @@ int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncar(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *memset(char *s, char b, unsigned int n);
+char *memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+#endif
