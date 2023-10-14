eca1cb990febc126baf751d4f967798c144e91ec
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-14 10:17:08 -0700

    betty checked

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,19 +1,27 @@
 #include "main.h"
+int prime(int n, int i);
 /**
- *
- *
- *
- *
+ *is_prime_number - states input as prime number or not
+ *@n: input value
+ *Return: 1 if n is prime, 0 if not
  */
 int is_prime_number(int n)
 {
-  int i = 2;
-  if (n == 0 || n == 1)
-    return (0);
-  if (n == i)
-    return (1);
-  if (n % i == 0)
-    return (0);
-  i++;
+if (n <= 1)
+return (0);
+return (prime(n, n - 1));
 }
-  is_prime_number(n);
+/**
+ *prime - checks input if it is prime
+ *@n: input 
+ *@i: input to iterate
+ *Return: 1 if n is prime, 0 if not
+ */
+int prime(int n, int i)
+{
+if (i == 1)
+return (1);
+if n % i == 0 && i > 0)
+return (0);
+return (prime(n, i - 1));
+}
