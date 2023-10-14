f11f0740bb615b7dd5ae4c5675521ad05cebc710
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-14 10:10:09 -0700

    test

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,11 +9,11 @@ int is_prime_number(int n)
 {
   int i = 2;
   if (n == 0 || n == 1)
-    Return (0);
+    return (0);
   if (n == i)
-    Return (1);
+    return (1);
   if (n % i == 0)
-    Return (0);
+    return (0);
   i++;
   is_prime_number(n);
 }
