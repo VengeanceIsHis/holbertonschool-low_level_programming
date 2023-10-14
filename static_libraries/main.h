934678c6a267c641bd9b9fe7611eb82a2e1aa9c4
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-14 16:09:05 -0700

    fix

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -14,8 +14,8 @@ char *_strcat(char *dest, char *src);
 char *_strncar(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-char *memset(char *s, char b, unsigned int n);
-char *memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
