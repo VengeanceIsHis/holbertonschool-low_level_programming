40361659b1d775641c53ab8538cf51842caf308d
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-14 08:53:53 -0700

    new test

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int _sqrt(int n, int i);
 /**
 *
 *
@@ -8,13 +9,23 @@
 */
 int _sqrt_recursion(int n)
 {
-  int i = 0;
-  int j = n / 2;
-  if (j != i)
-    {
-      i = j;
-      j = (n / i + i) / 2;
-    }
-  return (j);
-  _sqrt_recursion(n);
-    }
+  if (n < 0)
+    return (-1);
+  return (_sqrt(n, 0));
+}
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+int _sqrt(int n, int i)
+{
+  if (i * i > n)
+    return (-1);
+  if (i * i == n)
+    return (i);
+  return(_sqrt(n, i + 1));
+}
