922257e16b761df1cbf2d41caf82e3a4820751b0
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-15 15:31:41 -0700

    betty checked

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,27 +1,40 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
- *
- *
- *
- *
+ *main - adds only positive numbers
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: Always 0
  *
  */
 int main(int argc, char *argv[])
 {
-int a = 0;
-int b = 0;
-if (argc == 3)
+int i;
+unsigned int j, sum = 0;
+char *e;
+if (argc > 1)
 {
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-printf("%d\n", a +b);
-}
-else
+for (i = 1; i < argc; i++)
+{
+e = argv[i];
+for (j = 0; j < strlen(e); j++)
+{
+if (e[j] < 48 || e[j] > 57)
 {
 printf("Error");
 return (1);
 }
- return (0);
+}
+sum += atoi (e);
+e++;
+}
+printf("%d\n", sum);
+}
+else
+{
+printf("0\n");
+}
+return (0);
 }
