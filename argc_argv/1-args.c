824daa200c4f4e0f7afeca8c4e04a60ffe9f2fb8
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-15 14:39:43 -0700

    betty checked

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - prints argument count
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+(void) *argv;
+printf("%d\n", argc);
+return (0);
+}
