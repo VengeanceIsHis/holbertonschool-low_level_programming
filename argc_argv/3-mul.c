628d3252decfe4f67c45ec4047464399249402fd
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-15 15:01:24 -0700

    betty checked

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,15 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
- *
- *
+ *main - multiplies 2 argument numbers
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: Always (0)
  */
 int main(int argc, char *argv[])
 {
-  printf("%d\n", argv[0] * argv[1]);
-  return (0);
+int a = 0;
+int b = 0;
+if (argc == 3)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a * b);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
 }
