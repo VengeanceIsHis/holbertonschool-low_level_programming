34c5bae83050623b34628735572748b9f424aa5f
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-15 15:11:39 -0700

    test

---
+++ argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int main(int argc, char *argv)
+{
+int a = 0;
+int b = 0;
+if (argc == 3)
+{
+a = atoi(argv[1];
+b = atoi(argv[2];
+printf("%d\n", a +b);
+}
+else
+{
+printf("Error");
+return (1);
+}
+return (0)
+}
