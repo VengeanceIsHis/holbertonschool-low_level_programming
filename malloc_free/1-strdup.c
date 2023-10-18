899b80a8f6c67ba95ac8165a3bba3a5f5beddd43
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-18 12:27:37 -0700

    betty fix

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,30 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ *_strdup - copies a string
+ *@str: the string input
+ *Return: the string duplicate
  */
 char *_strdup(char *str)
 {
-  int a = 0;
-  int i = 1;
-  char *s;
-  if (str == NULL)
-      return (NULL);
-  while (str[i])
-    {
-      i++;
-    }
-  s = malloc((sizeof(char) * i) + 1);
-  if (s == NULL)
-    return (NULL);
-  while (a < i)
-    {
-      s[a] = str[a];
-      a++;
+int a = 0;
+int i = 1;
+char *s;
+if (str == NULL)
+return (NULL);
+while (str[i])
+{
+i++;
 }
-  s[a] = '\0';
-  return (s);
+s = malloc((sizeof(char) * i) + 1);
+if (s == NULL)
+return (NULL);
+while (a < i)
+{
+s[a] = str[a];
+a++;
+}
+s[a] = '\0';
+return (s);
 }
