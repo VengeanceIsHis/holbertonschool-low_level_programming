bf5fd0307f9fa39b7e86eb3cffd5ab17b813e2b8
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-18 11:42:31 -0700

    add

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+int i;
+char *s;
+if (size == 0)
+return (NULL);
+s = malloc(size * sizeof(char));
+if (s == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+{
+s[i] = c;
+}
+return (s);
+}
