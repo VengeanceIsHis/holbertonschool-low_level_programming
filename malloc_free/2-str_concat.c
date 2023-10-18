1d8c7e88b2bb91023a162d7149cd4ad1a6b08d78
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-18 13:54:28 -0700

    fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,11 +13,11 @@ int i = 0, j = 0, k = 0, l = 0;
 char *s;
 if (s1 == NULL)
 s1 = "";
-else if (*s2 == NULL)
+if (s2 == NULL)
 s2 = "";
 while (s1[i])
 i++;
-while s2[j])
+while (s2[j])
 j++;
 l = i + j;
 s = malloc((sizeof(char) *l) +1);
