20b92758142b8f2c1db7ac6487b40707808d4aba
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-19 08:29:19 -0700

    test

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+int **alloc_grid(int width, int height)
+{
+  int **array[width][height];
+  if (width = 0 || height = 0)
+    return (NULL);
+      return (0);
+}
