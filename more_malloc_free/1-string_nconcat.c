46c58aaa4a5f1de0f1c106117d9e6f9319b8d9f2
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-21 13:24:36 -0700

    test

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ */
+char *string_nconcat(char *s1, char *s2, unisgned int n)
+{
+  int a;
+  int b;
+  int c;
+  int d;
+  char *s;
+  if (s1 == NULL)
+    s1 = "";
+  if (s2 == NULL)
+      s2 = "";
+  while (s1[a])
+    a++;
+  while (s2[b] < n)
+    b++;
+  d = a + b;
+  s = malloc((sizeof(char) * d) +1);
+  if (s == NULL)
+    return (NULL);
+  b = 0;
+  while (c < d)
+    {
+      if (c <= a)
+	s[c] = s1[c];
+      if (c >= a)
+	{
+	  s[k] = s2[b];
+	  b++;
+	}
+      c++;
+}
+s[c] = '\0';
+return (s);
