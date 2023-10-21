a01400b8161ab7f4fc76f3641e7dfc5d9d83a6f4
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-21 13:44:35 -0700

    test

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
   d = a + c;
   else
     d = a + n;
-  s = malloc((sizeof(char) * d +1));
+  s = malloc(sizeof(char) * d +1);
   if (s == NULL)
     return (NULL);
   c = 0;
