b97493c11d987b7029d15405500c583428c0db0f
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-21 15:15:25 -0700

    test

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_calloc - 
+ *
+ *
+ */
+void *_calloc(unsigned int nmeb, unsigned int size);
+{
+int a = 0, m = 0;
+char *s;
+if (nmemb == 0 || size == 0)
+return (NULL);
+m = nmemb * size;
+s = malloc(m);
+if (s == NULL)
+return (NULL);
+while (a < m)
+{
+s[a] = 0;
+a++;
+}
+return (s);
+}
