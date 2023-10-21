cd1b6fda0e97255b54635419b201a3a0a8408a3a
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-21 13:42:56 -0700

    fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -6,10 +6,10 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  int a;
+  unsigned int a;
  unsigned int b;
-  int c;
-  int d;
+  unsigned int c;
+  unsigned int d;
   char *s;
   if (s1 == NULL)
     s1 = "";
@@ -19,7 +19,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
     a++;
   while (s2[c])
       c++;
+  if (n >= c)
   d = a + c;
+  else
+    d = a + n;
   s = malloc((sizeof(char) * d +1));
   if (s == NULL)
     return (NULL);
