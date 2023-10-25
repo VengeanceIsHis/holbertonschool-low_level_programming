ec7e7b0d9ba49a59caf9462206b8bd7a460b83e5
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-25 11:48:44 -0700

    betty test

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,7 +2,11 @@
 #define doggy_h
 #include <stdio.h>
 /**
- *defines new type named dog
+ *struct dog - Name, age, owner of a dog
+ *@name: The name of the dog
+ *@age: The age of the dog
+ *@owner: The owner of the dog
+ *Description: States the attributes of the dog
  */
 struct dog
 {
