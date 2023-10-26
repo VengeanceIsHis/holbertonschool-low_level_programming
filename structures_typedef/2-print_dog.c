190601cba902fa8340459fc33e3233ad3fc92678
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-26 11:59:07 -0700

    betty fix

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- *
- *
- *
+ *print_dog - function that prints all attributes of given dog
+ *@d: a dog structure
+ *Return: void
  */
 void print_dog(struct dog *d)
 {
-  if (*d = NULL)
-    {
-printf("(nil)")
+if (d != NULL)
+{
+printf("Name: %s\n", (d->name) ? d->name : "(nil)" );
+printf("Age: %s\n", (d->age) ? d->age : 0);
+printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 }
-printf("Name: %s", d->name);
-  printf("Age: %s", d->age);
-  printf("Owner: %s", d->owner);
 }
