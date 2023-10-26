77f3a46047fd80bf440fef67f99e2989fca16781
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-26 11:09:17 -0700

    betty fix

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- *
- *
- *
+ *init_dog - function that initialize a variable of type struct dog
+ *@d: dog structure
+ *@name: Name of the dog
+ *@age: The age of the dog
+ *@owner: The owner of the dog
+ *Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
@@ -11,6 +14,6 @@ if (d != NULL)
 {
 d->name = name;
 d->age = age;
-      d->owner = owner;
-          }
-             }
+d->owner = owner;
+}
+}
