ac895fc08231eeeba5826743dabb0bcbf56fb230
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-26 11:47:52 -0700

    test

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ *
+ *
+ *
+ */
+void print_dog(struct dog *d)
+{
+  if (d != NULL)
+    printf("Name: %s", d->name);
+  printf("Age: %s", d->age);
+  printf("Owner: %s", d->owner);
+}
