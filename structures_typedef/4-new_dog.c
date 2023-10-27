23d294a0f40cac072a862b6c15454ba6d9cd632e
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-27 13:14:08 -0700

    test

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -47,8 +47,9 @@ dog_t *new_dog(char *name, float age, char *owner)
  *
  */
 int _strlen(char *s)
-  int c;
-for (c = 0; *s != '\0'; s++)
+{
+  int c = 0;
+for (; *s != '\0'; s++)
   {
     c++;
   }
