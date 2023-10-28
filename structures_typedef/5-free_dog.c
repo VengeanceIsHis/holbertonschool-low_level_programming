250498918e829e733ca95eb7d1ff234c3974e12c
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-27 19:43:22 -0700

    betty fix

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,17 +1,16 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- *
- *
- *
- *
+ *free_dog - function that frees the memory space of dog
+ *@d: input value to be freed
+ *Return: void
  */
 void free_dog(dog_t *d)
 {
-  if (d != NULL)
-    {
-      free(d->owner);
-  free(d->name);
-  free(d);
-    }
+if (d != NULL)
+{
+free(d->owner);
+free(d->name);
+free(d);
+}
 }
