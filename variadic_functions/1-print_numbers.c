f659a4f400dcdfe928929b9d388842abbc77d361
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-29 13:55:28 -0700

    fix

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,23 +3,23 @@
 #include <stdlib.h>
 #include <stdarg.h>
 /**
- *
- *
- *
- *
- *
+ *print_numbers - function that prints all given numbers followed by given string
+ *@n: number of paramers to be added to function
+ *@...: infinite variables to be added to function
+ *@seperator: string to add in between each variable
+ *Return: void
  */
 void print_numbers(const char *seperator, const unsigned int n, ...)
 {
-  va_list ap;
-  unsigned int i;
-	va_start(ap, n);
-  for (i = 0; i < n; i++)
-    {
-      printf("%d", va_arg(ap, int));
-      if (i != (n - 1) && seperator != NULL)
-	   printf("%s", seperator);
-    }
-  printf("\n");
-  va_end(ap);
-	   }
+va_list ap;
+unsigned int i;
+va_start(ap, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(ap, int));
+if (i != (n - 1) && seperator != NULL)
+printf("%s", seperator);
+}
+printf("\n");
+va_end(ap);
+}
