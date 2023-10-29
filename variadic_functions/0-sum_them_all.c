b8f5ea0391e7deea23d219f99a5ef32399a9f2ae
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-29 13:32:46 -0700

    test

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlb.h>
+#include <stdarg.h>
+/**
+ *sum_them_all - function that adds all optional numbers given up to n
+ *@n: max amount of input numbers to enter
+ *@...: A variable number of input for sum
+ *Return 0 if n is 0 else sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list ap;
+unsigned int i, sum = 0;
+if (n == 0)
+{
+return (0)
+}
+va_start(ap , n);
+for (i = 0; i < n; i++)
+sum += va_arg(ap, int);
+va_end(ap); 
+return (sum);
+}
--- variadic_functions/variadic.functions.h
+++ variadic_functions/variadic.functions.h
@@ -1,3 +1,5 @@
 #ifndef main_h
 #define main_h
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+#endif
