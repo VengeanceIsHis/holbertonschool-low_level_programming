7a16004b12255a9bd71b1e5c852313deea497a5b
VengeanceIsHis <elijahjpoole@gmail.com>
2023-10-29 13:52:28 -0700

    test

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,14 +13,13 @@ void print_numbers(const char *seperator, const unsigned int n, ...)
 {
   va_list ap;
   unsigned int i;
-  if (n == 0)
-    printf(" ");
-      if (seperator != NULL)
 	va_start(ap, n);
   for (i = 0; i < n; i++)
     {
       printf("%d", va_arg(ap, int));
+      if (i != (n - 1) && seperator != NULL)
 	   printf("%s", seperator);
-	   va_end(ap);
+    }
+  printf("\n");
+  va_end(ap);
 	   }
-}
