7a272dc9eeaa9c8c9be3bbbc367df3d511aa56a2
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-08 08:50:30 -0800

    fix

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,25 +4,25 @@
 #include <stddef.h>
 #include <string.h>
 /**
- *
- *
- *
+ *print_list - function that prints the elements of list_t nodes
+ *@h: pointer to each node and its data
+ *Return: Nodes added
  */
 size_t print_list(const list_t *h)
 {
-  size_t nodes = 0;
-  while (h)
-    {
-      if (h->str != NULL)
-    {
-      printf("[%d] %s\n", h->len, h->str);
-    }
-  else
-    {
-      printf("[0] (nil)\n");
-    }
-      nodes++;
-      h = h->next;
+size_t nodes = 0;
+while (h)
+{
+if (h->str != NULL)
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+else
+{
+printf("[0] (nil)\n");
+}
+nodes++;
+h = h->next;
 }
 return (nodes);
 }
