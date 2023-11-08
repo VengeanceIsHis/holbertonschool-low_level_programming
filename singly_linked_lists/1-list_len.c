1ff2746b937331917f319938fc4ddd2d5837f5b9
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-08 11:34:25 -0800

    fix

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * 
- *
+ *list_len - function that lists the elements passed to the function
+ *@h: input head node
+ *Return: number of elements in the list
  */
 size_t list_len(const list_t *h)
 {
