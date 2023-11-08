ed7238e45cea734235e71166e4c137b64fa9a7f4
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-08 08:42:41 -0800

    fixed

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -16,13 +16,13 @@ size_t print_list(const list_t *h)
       if (h->str != NULL)
     {
       printf("[%d] %s\n", h->len, h->str);
-      nodes++;
-      h= h->next;
     }
   else
     {
       printf("[0] (nil)\n");
     }
+      nodes++;
+      h = h->next;
 }
 return (nodes);
 }
