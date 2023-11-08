f271af6adfb9afd7d03aa2e998c9a6372ba658da
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-08 12:24:40 -0800

    betty fix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -5,21 +5,21 @@
 /**
  *add_node - function that adds nodes as the head
  *@head: input list
- *@str: string input 
+ *@str: string input
  *Return: Address of list or NULL if empty
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *push;
-	int length = 0;
-	push = malloc(sizeof(list_t));
-	if (push == NULL)
-		return (NULL);
-	while (str[length])
-		length++;
-	push->len = length;
-	push->str = strdup(str);
-	push->next = *head;
-	*head = push;
-	return (push);
+list_t *push;
+int length = 0;
+push = malloc(sizeof(list_t));
+if (push == NULL)
+return (NULL);
+while (str[length])
+length++;
+push->len = length;
+push->str = strdup(str);
+push->next = *head;
+*head = push;
+return (push);
 }
