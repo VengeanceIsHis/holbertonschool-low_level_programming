5a2d1921cb634f3d6d33a3d7f4bc0589780f09a5
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-08 08:35:58 -0800

    I want these files Revert "test"
    This reverts commit cd89dc88b18a44d2fe627fc36e1e81a719ae0e38.

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -7,7 +7,7 @@
  *@f: pointer to function
  *Return: void
  */
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *)
 {
 if (name != NULL && f != NULL)
 f(name);
