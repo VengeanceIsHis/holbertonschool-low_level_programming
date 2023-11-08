720db2d3755caf173bd714bace30513383d2ca89
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-08 11:50:47 -0800

    test

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *
+ *
+ */
+list_t *add_node(list_t **head, const char *str
+		{
+		list_t push;
+		push->str = str;
+		push->head = (*head);
+		(*head) = list_s
+		}
