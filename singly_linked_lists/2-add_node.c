04380c479b0d3d3d2c10d9a8c2b2fbe4d6ce300f
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-08 12:22:03 -0800

    fix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,18 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- *
- *
+ *add_node - function that adds nodes as the head
+ *@head: input list
+ *@str: string input 
+ *Return: Address of list or NULL if empty
  */
 list_t *add_node(list_t **head, const char *str)
 {
-		list_t *push;
-		int length = 0;
-		push = malloc(sizeof(list_t));
-		if (push == NULL)
-			return (NULL);
-		while (str[length])
-			length++;
+	list_t *push;
+	int length = 0;
+	push = malloc(sizeof(list_t));
+	if (push == NULL)
+		return (NULL);
+	while (str[length])
+		length++;
 	push->len = length;
 	push->str = strdup(str);
 	push->next = *head;
