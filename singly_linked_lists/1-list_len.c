990a62cf3ee2b3a1e9c709983da6cc06d27b2ce4
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-08 11:15:27 -0800

    test

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * 
+ *
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements;
+	while (h)
+	{
+		if (h->str && h->len)
+	{
+		elements = elements + 2;
+	}
+	h->next;
+	}
+	return
+}
