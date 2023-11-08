790a5e959ad5bbe6551a0f3b51e3f799b4847c41
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-08 07:52:58 -0800

    tetst

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+size_t print_list(const list_t *h)
+{
+  char *str = h;
+  unsigned int len;
+  if (str == NULL)
+    printf("[0] (nil)");
+  else
+    len = strlen(str);
+  return (str);
+}
