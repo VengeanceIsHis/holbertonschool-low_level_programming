5156ac186364f0c53bc66d41dc59a948385f1a17
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-09 07:59:21 -0800

    test

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,12 +12,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *push;
 	list_t *current_node;
 	size_t n;
-	push = (list_t *)malloc(sizeof(list_t));
+	push = malloc(sizeof(list_t));
 	if (push == NULL)
 		return (NULL);
 	push->str = strdup(str);
 	for (n = 0; str[n]; n++)
-		;
 	push->len = n;
 	push->next = NULL;
 	current_node = *head;
