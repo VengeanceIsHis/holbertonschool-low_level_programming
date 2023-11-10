942fc82d86da49328d8990490735b27fe354b7d4
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-10 07:31:54 -0800

    fix

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- *
- *
- *
+ *free_list - function that frees a node and its data
+ *@head: the node to be freed
+ *Return: void
  */
 void free_list(list_t *head)
 {
