5cef349504e1f8295ffd942819504d35716ec773
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-10 08:29:11 -0800

    fix

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -2,9 +2,10 @@
 #define main_h
 #include <stddef.h>
 /**
- *
- *
- *
+ *struct listint_s - singly linked list
+ *@n: integer
+ *@next: points to the next node
+ *Description: singly linked list node structure
  */
 typedef struct listint_s
 {
