61dd3f87b50c0d83aa31aecb9000e5ae0561bfb7
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-10 07:29:13 -0800

    betty fix

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,29 +9,29 @@
  *Return: The address of the inputted node */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *push;
-	list_t *current_node;
-	push = malloc(sizeof(list_t));
-	if (push == NULL)
-		return (NULL);
-	push->str = strdup(str);
-	if (push->str == NULL)
-	{
-		free(push);
-		return (NULL);
-	}
-	push->len = strlen(push->str);
-	push->next = NULL;
-	if (*head == NULL)
-	{
-		*head = push;
-		return (push);
-	}
-	current_node = *head;
-	while (current_node->next)
-	{
-		current_node = current_node->next;
-	}
-	current_node->next = push;
-	return (push);
+list_t *push;
+list_t *current_node;
+push = malloc(sizeof(list_t));
+if (push == NULL)
+return (NULL);
+push->str = strdup(str);
+if (push->str == NULL)
+{
+free(push);
+return (NULL);
+}
+push->len = strlen(push->str);
+push->next = NULL;
+if (*head == NULL)
+{
+*head = push;
+return (push);
+}
+current_node = *head;
+while (current_node->next)
+{
+current_node = current_node->next;
+}
+current_node->next = push;
+return (push);
 }
