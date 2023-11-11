8d68da6beb77aa4a155396192620378afc8bccd9
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-11 14:04:20 -0800

    fix

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -6,15 +6,13 @@
 size_t listint_len(const listint_t *h)
 {
   size_t count;
-  size_t i = 0;
   if (h == NULL)
 {
   return (0);
  }
   for (count = 0; h != NULL; count++)
     {
-      i += count;
 	h = h->next;
     }
-  return (i);
+  return (count);
 }
