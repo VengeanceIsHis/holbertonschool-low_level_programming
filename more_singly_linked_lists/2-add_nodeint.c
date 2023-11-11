efb0591606f05137c13921db90cc9dc00076c072
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-11 14:24:36 -0800

    fix

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -7,7 +7,7 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *push;
-		int length = 0;
+	listint_t length = 0;
 	push = malloc(sizeof(listint_t));
 	if (push == NULL)
 		return (NULL);
