91cc0bd6f72be5c15d6390f5349961f030789b4f
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-11 14:07:26 -0800

    fix

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,19 @@
 #include "lists.h"
 /**
- *
- *
+ *listint_len - returns the number of elements in list
+ *@h: pointer to first node
+ *Return: Elements in list
  */
 size_t listint_len(const listint_t *h)
 {
-  size_t count;
-  if (h == NULL)
+size_t count;
+if (h == NULL)
 {
-  return (0);
- }
-  for (count = 0; h != NULL; count++)
-    {
-	h = h->next;
-    }
-  return (count);
+return (0);
+}
+for (count = 0; h != NULL; count++)
+{
+h = h->next;
+}
+return (count);
 }
