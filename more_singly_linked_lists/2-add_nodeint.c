2a31ad6d8024f13a2f4ebb2d507f361babfc215c
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-11 14:19:39 -0800

    test

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * 
+ *
+ *
+ */
+listint_t *addnodeint(list_t **head, const int n);
+{
+	list_t *push
+		int length = 0;
+	push = malloc(sizeof(listint_t));
+	if (push == NULL)
+		return (NULL);
+	push->n = length;
+	push->next = *head;
+	*head = push;
+	return (length);
+}
