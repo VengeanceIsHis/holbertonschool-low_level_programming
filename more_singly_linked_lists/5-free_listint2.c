c580887ca97c44b226fe0284f0fca0f6bf1aad7d
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-12 12:26:23 -0800

    fix

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ *free_listint2 - function that frees all nodes in a list
+ *@head: input of the first node to be freed
+ *Return: void
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
-	if (head == NULL|| *head == NULL)
+listint_t *tmp;
+if (head == NULL|| *head == NULL)
 return;
 while ((*head)->next != NULL)
 {
