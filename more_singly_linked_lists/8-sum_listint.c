b3d9a574768e19c46daa212d444faee777515dab
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-12 14:19:59 -0800

    fix

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -6,16 +6,14 @@
  */
 int sum_listint(listint_t *head)
 {
-
-	int sum = 0;
-	int count;
-	if (head == NULL)
-
-		return (0);
-	for (count = 0; head != NULL; count++)
-	{
-		sum += head->n;
-	head = head->next;
-	}
-	return (sum);
+int sum = 0;
+int count;
+if (head == NULL)
+return (0);
+for (count = 0; head != NULL; count++)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
 }
