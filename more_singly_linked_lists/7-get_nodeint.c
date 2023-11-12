518b0d977fe5362ccea8aefd4c7840070678dd04
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-12 13:57:54 -0800

    test

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -8,15 +8,14 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *tmp;
 	unsigned int count;
 	if (head == NULL)
 		return (NULL);
 	for (count = 0; count < index; count++)
 	{
-		if (head->next != NULL)
-			tmp = (*head)->next;
-		head = tmp;
+		if (head == NULL)
+		return (NULL);
+		head = head->next;
 	}
 	return (head);
 	
