f7d764cfa5ce49aaa7f6c931d06dd32a50ba71f3
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-12 09:49:48 -0800

    fix

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -1,23 +1,24 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * 
- *
- *
+ *add_nodeint - adds a note to the beginning of a list 
+ *@h: pointer to the first node
+ *@n: input data for the node
+ *Return: Address of the new node
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *push;
-	push = malloc(sizeof(listint_t));
-	if (head == NULL)
-		return (NULL);
-	if (push == NULL)
-		return (NULL);
-	if (*head == NULL)
-		push->next = NULL;
-	else 
-		push->next = *head;
-	push->n = n;
-	*head = push;
-	return (*head);
+listint_t *push;
+push = malloc(sizeof(listint_t));
+if (head == NULL)
+return (NULL);
+if (push == NULL)
+return (NULL);
+if (*head == NULL)
+push->next = NULL;
+else 
+push->next = *head;
+push->n = n;
+*head = push;
+return (*head);
 }
