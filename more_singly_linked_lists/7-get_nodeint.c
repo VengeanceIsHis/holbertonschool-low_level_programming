12e5655d04f0e1a9c1e68a2a455cb905b58c9b08
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-12 13:52:28 -0800

    fix

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int count;
 	if (head == NULL)
 		return (NULL);
-	tmp = (*head);
+	tmp = head;
 	for (count = 0; count < index; count++)
 	{
 		if (head->next != NULL)
