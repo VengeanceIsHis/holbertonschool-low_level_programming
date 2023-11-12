4119de2419e252ce90c8ec4cbf27c27a2e5908ad
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-12 14:02:37 -0800

    fix

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,22 +1,21 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ *get_nodeint_at_index - prints the data of the nth node
+ *@head: input head node
+ *@index: input to determine which node to return
+ *Return: the found node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int count;
-	if (head == NULL)
-		return (NULL);
-	for (count = 0; count < index; count++)
-	{
-		if (head == NULL)
-		return (NULL);
-		head = head->next;
-	}
-	return (head);
-	
+unsigned int count;
+if (head == NULL)
+return (NULL);
+for (count = 0; count < index; count++)
+{
+if (head == NULL)
+return (NULL);
+head = head->next;
+}
+return (head);	
 }
