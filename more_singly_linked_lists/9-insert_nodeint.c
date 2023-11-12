045c81aa446a0cdcf711fb6e45fc3532453543b3
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-12 14:37:11 -0800

    fix

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = malloc(sizeof(listint_t));
 	for (count = 0; count < idx; count++)
 	{
-		*head = *head->next;
+		*head = (*head)->next;
 	}
 	if (head = NULL)
 		tmp->next = NULL;
