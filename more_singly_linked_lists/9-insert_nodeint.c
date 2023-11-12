10f750d761b539751266d5e813de1cc496bbe178
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-12 14:38:23 -0800

    fix

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		*head = (*head)->next;
 	}
-	if (head = NULL)
+	if ((*head) = NULL)
 		tmp->next = NULL;
 	else
 		tmp->next = *head;
