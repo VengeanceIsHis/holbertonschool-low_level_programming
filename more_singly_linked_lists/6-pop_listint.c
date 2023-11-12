3031c69a86cad330e81ed3d239eed470485b7496
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-12 12:47:27 -0800

    fix

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -9,12 +9,12 @@
 int pop_listint(listint_t **head)
 {
 int n;
-listint_t tmp;
+listint_t *tmp;
 	if (*head == NULL || head == NULL)
 		return (0);
 	n = (*head)->n;
 	tmp = *head;
-		*head = *(head)->next;
+		*head = (*head)->next;
 		free(tmp);
 		return (n);
 }
