e619d9f9f0c532650a4d00bd15a22cbc12303d8f
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-12 13:46:30 -0800

    fix

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp;
+	unsigned int count;
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	tmp = (*head);
+	for (count = 0; count < index; count++)
+	{
+*head = *head->next
+	}
+	return (*head);
+	
+}
