950a2ad634446404358b0bd6537b1dca3b63630c
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-15 12:16:41 -0800

    test

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ *
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes;
+	dlistint_t tmp;
+	dlistint_t current;
+	if (h == NULL)
+		return (NULL);
+	else 
+		tmp = h;
+		while (tmp != NULL)
+		{
+	current = tmp->next;
+	printf("%d", tmp->n);
+nodes++;
+tmp = current;
+		}
---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,16 @@
+#ifndef main_h
+#define main_h
+/**
+ *
+ *
+ *
+ *
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+size_t print_dlistint(const dlistint_t *h);
+#endif
