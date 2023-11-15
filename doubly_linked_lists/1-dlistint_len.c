4b236f9010d0fc889c21172efc3ef2eca69ee710
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-15 15:17:32 -0800

    test

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+ *
+ *
+ *
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t el = 0;
+
+	if (h == NULL)
+		return (ele);
+while (h->prev != NULL)
+	h = h->prev;
+while (h != NULL)
+{
+	if (h->n)
+		el++;
+	h = h->next;
+}
+	return (nodes);
+	}
