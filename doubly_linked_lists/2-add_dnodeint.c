cea7d0f98b3cbaef1722ae69485c68bf3e62d8ef
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-15 18:39:05 -0800

    fix

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
-	tmp = *head
+	tmp = *head;
 	if (tmp != NULL)
 	{
 		while (tmp->prev != NULL)
