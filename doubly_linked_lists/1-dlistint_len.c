d1257d1136f7a1cef6c84f28b3fed70998207cc4
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-15 18:16:59 -0800

    fix

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 #include <stddef.h>
 /**
- *
- *
- *
+ *dlistint_len - returns the number of elements in a list
+ *@h: pointer to a node
+ *Return: the number of elements
  */
 size_t dlistint_len(const dlistint_t *h)
 {
@@ -11,12 +11,12 @@ size_t dlistint_len(const dlistint_t *h)
 
 	if (h == NULL)
 		return (el);
-while (h->prev != NULL)
-	h = h->prev;
-while (h != NULL)
+	while (h->prev != NULL)
+		h = h->prev;
+		while (h != NULL)
 {
-		el++;
+	el++;
 	h = h->next;
 }
 	return (el);
-	}
+}
