53e7bffd24a909a2f62839878e9e85bb3753c02a
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-17 12:02:35 -0800

    test

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *tmp;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	tmp = *head;
+	if (tmp != NULL)
+	{
+	while (tmp->prev != NULL)
+		tmp = tmp->prev;
+	}
+	for (count = 0; new != NULL; count++)
+	{
+		new = new->next;
+	}
+	new->n = n;
+	while (tmp != new)
+	{
+		tmp = tmp->next
+	}
+	tmp->next = new;
+	new->prev = tmp;
+	*head = new;
+	return (new);
+
