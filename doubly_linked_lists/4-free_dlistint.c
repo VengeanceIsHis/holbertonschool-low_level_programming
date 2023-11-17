e26cc6916c2e17b18e8e640c2588d31871e7bb77
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-17 12:56:33 -0800

    fix

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- *
- *
+ *free_dlistint - frees a list
+ *@head: pointer to the first node in a list
+ *Return: void
  */
 void free_dlistint(dlistint_t *head)
 {
