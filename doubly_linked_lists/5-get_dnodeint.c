07c78ee4e26c8f31fb011eaf6bc8c44827a31b24
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-17 13:20:17 -0800

    kernel fi

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ *get_dnodeint_at_index - navigates through list and returns nth node
+ *@head: pointer to the first node
+ *@index: indicator for the node to return
+ *Return: The node that corresponds with index
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
@@ -11,7 +12,7 @@ unsigned int count = 0;
 dlistint_t *tmp;
 
 if (head == NULL)
-		return (NULL);
+	return (NULL);
 tmp = head;
 while (tmp)
 {
