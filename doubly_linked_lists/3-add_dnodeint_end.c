dca416c9484c2359a133dd74b7a14d0a94770446
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-17 12:49:53 -0800

    kernel fix

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,34 +1,33 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- *
- *
- *
- *
- *
+ *add_dnodeint_end - adds a node to the end of a list
+ *@head: pointer to a node in a list
+ *@n: data input for node
+ *Return: address of inputted node
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
-	dlistint_t *tmp;
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->n = n;
-	new->next = NULL;
-	tmp = *head;
-	if (tmp != NULL)
-	{
-	while (tmp->next != NULL)
-		tmp = tmp->next;
-	tmp->next = new;
-	}
-	else
-	{
-		*head = new;
-	}
-	new->prev = tmp;
-	return (new);
+dlistint_t *new;
+dlistint_t *tmp;
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+{
+return (NULL);
+}
+new->n = n;
+new->next = NULL;
+tmp = *head;
+if (tmp != NULL)
+{
+while (tmp->next != NULL)
+tmp = tmp->next;
+tmp->next = new;
+}
+else
+{
+*head = new;
+}
+new->prev = tmp;
+return (new);
 }
