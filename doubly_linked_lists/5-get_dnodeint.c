34d8db019d50021dcd6eca7d36632a06823e9df5
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-17 13:08:08 -0800

    fix

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -6,11 +6,12 @@
  */
 dlistint_t *get_dnode_at_index(dlistint_t *head, unsigned int index)
 {
-	if (head == NULL)
+int count;
+if (head == NULL)
 		return (NULL);
 	for (count = 0; count < index; count++)
 	{
-		head = head->next
+		head = head->next;
 	}
 	return (head);
 }
