c0bf0d312e1164a0baf9d3ff5956032bf7478a9c
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-18 12:23:55 -0800

    test

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp;
+	dlistint_t *new;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+tmp = *h;
+if (tmp != NULL)
+	for (count = 0; count < idx; count++)
+	{
+		tmp = tmp->next;
+}
+new = tmp;
+new->prev = tmp;
+new->next = current;
+for (i = 0; current != NULL; i++)
+{
+	current = current->next
+}
+else 
+{
+*h = new;
+}
+return (new);
+}
