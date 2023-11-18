7d86237ee1ef7060cfd0fe4a85da1165e69a5343
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-18 13:11:35 -0800

    fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,6 +11,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int count;
 	dlistint_t *tmp;
 	dlistint_t *new;
+
+	new = NULL;
 if (idx == 0)
 	new = add_dnodeint(h, n);
 else
