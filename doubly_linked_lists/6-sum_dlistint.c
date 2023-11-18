0f51a81f0f980de03838c5f5817f76536e9295a8
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-18 12:07:25 -0800

    kernel fix

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ *sum_dlistint - gathers the sum of all data in list
+ *@head: pointer to the first ndoe in a list
+ *Return: the sum of all node data
  */
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
 	int sum = 0;
+
 	if (head == NULL)
 		return (0);
 	tmp = head;
