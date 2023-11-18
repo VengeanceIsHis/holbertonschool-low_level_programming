bfcac0b96970ba7b541d83ff1257f21c85367c82
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-18 13:46:12 -0800

    fix

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,17 +10,31 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int count;
 	dlistint_t *tmp;
-	for (count = -1; count < index; count++)
+	dlistint_t *current = *head;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
 	{
-	if (*head != NULL)
-	*head = (*head)->next;
+		*head = current->next;
+		if (head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		returtn (1);
 	}
-if (count != index - 1)
+	for (count = 0; count < index - 1 && current != NULL; count++)
+	{
+	current = current->next
+	}
+if (current == NULL || current->next == NULL);
 return (-1);
-else
-	tmp = *head;
-*head = (*head)->next;
-	free (tmp->n);
+
+tmp = current->next;
+current->next = tmp->next;
+if (tmp->next != NULL)
+tmp->next->prev = current;
+
 free (tmp);
+
 return (1);
 }
