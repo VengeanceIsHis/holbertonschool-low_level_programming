5dfbcc65232f5d6153bf1445322910cf454a95e1
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-21 19:01:46 -0800

    betty fix

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,26 +2,24 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ *hash_table_create - creates a hash table 
+ *@size: size of hash table
+ *Return: Address of new hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash;
-	
-	if (size == 0)
-		return (NULL);
-	hash = malloc(sizeof(hash_table_t));
-	if (hash == NULL)
-		return (NULL);
-	hash->size = size;
-	hash->array = calloc(size, sizeof(hash_node_t *));
-	if (hash->array == NULL)
-	{
-		free(hash);
-		return (NULL);
-	}
-	return (hash);
+hash_table_t *hash;	
+if (size == 0)
+return (NULL);
+hash = malloc(sizeof(hash_table_t));
+if (hash == NULL)
+return (NULL);
+hash->size = size;
+hash->array = calloc(size, sizeof(hash_node_t *));
+if (hash->array == NULL)
+{
+free(hash);
+return (NULL);
+}
+return (hash);
 }
