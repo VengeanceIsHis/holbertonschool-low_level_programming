f0a40b86c2928ff473b595a438cbf2bcbe758c44
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-21 19:40:01 -0800

    fix

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,11 +1,10 @@
 #ifndef main_h
 #define main_h
 /**
- *
- *
- *
- *
- *
+ *struct hash_node_s - Node of a hash table
+ *@key: The key, string
+ *@value: The value corresponding to a key
+ *@next: A pointer to the next node
  */
 typedef struct hash_node_s
 {
@@ -14,11 +13,9 @@ typedef struct hash_node_s
 	struct hash_node_s *next;
 } hash_node_t;
 /**
- *
- *
- *
- *
- *
+ *struct hash_table_s - Hash table data structure
+ *@size: The size of the array
+ *@array: an array of size @size
  */
 typedef struct hash_table_s
 {
