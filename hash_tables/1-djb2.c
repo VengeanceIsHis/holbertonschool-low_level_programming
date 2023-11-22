c5f2ab4e71d31e098264e3b0443a765de8c3f0e2
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-21 20:38:07 -0800

    fin

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -4,7 +4,7 @@
  *@str: input value
  *Return: value of hash
  */
-unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
