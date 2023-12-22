742796ebcf9e37557229fc1545a9979919c7c552
VengeanceIsHis <elijahjpoole@gmail.com>
2023-12-22 09:53:48 -0800

    fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,21 +6,16 @@
 int linear_search(int *array, size_t size, int value)
 {
   size_t i;
-  int result = 0;
   for (i = 0; i < size; i++)
     {
       printf("Value checked array[%lu] = [%d]\n", i, array[i]);
       if (array[i] == value)
-	{
-	  result = 1;
+      {
 	  printf("Found %d at index: %lu\n", value, i);
-	  break;
+	  return (value);
 	}
       else
 	continue;
     }
-  if (result == 1)
-	return (value);
-  else
-    return (-1);
+return (-1);
 }
