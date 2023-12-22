ddefa8feae5df3c978d889dfcd8344748bf0a98b
VengeanceIsHis <elijahjpoole@gmail.com>
2023-12-22 09:59:37 -0800

    fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,7 +10,7 @@ int linear_search(int *array, size_t size, int value)
     {
       printf("Value checked array[%lu] = [%d]\n", i, array[i]);
       if (array[i] == value)
-	  return (value);
+	  return (i);
     }
 return (-1);
 }
