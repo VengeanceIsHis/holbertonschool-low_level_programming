1092b25a902d01003d81b622bd31fa2e8e9258e8
VengeanceIsHis <elijahjpoole@gmail.com>
2023-12-22 18:19:43 -0800

    fix

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -9,8 +9,9 @@ int binary_search(int *array, size_t size, int value)
   size_t i, mid, hsize = size, power, exponent, term;
   int *hold = array;
   int result = -1, ans = 0;
+  while (result > 0)
+    {
       printf("Searching in array: ");
-      while (result > 1)
 	{
 	  for (i = 0; i < hsize; i++)
 	{
